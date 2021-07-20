60240426ef08617b7cdac8f4cabbc21897a75793
dassader <spam@dassader.com>
2021-07-20 22:15:00 +0400

    Init

---
+++ src/main/java/com/example/removeme/RemoveMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.removeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoveMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoveMeApplication.class, args);
+    }
+
+}
